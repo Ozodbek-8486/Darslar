491439ef65fb4b61f57d9ae0ea3279098d19aabb
ozodbek7767 <ozodbekusmonqulov7777@gmail.com>
2025-02-24 17:02:40 +0500

    f

---
+++ Python-10/python.py
@@ -0,0 +1,137 @@
+    # 1-topshiriq:
+
+# f  = {
+# "brand" : "Ford",
+# "electric":False,
+# "year" : 1964,
+# "colors":["red","white","black"],
+# }
+
+# for t,e, in f.items():
+#     if t == "colors":
+#         print(f"{t}")
+#         for rang in e:
+#             print(f"   -{rang}")
+#     else:
+        # print(f"{t} {e}")
+
+# f = {
+# "Ali": 85, 
+# "Vali": 92, 
+# "Hasan": 78,
+#  "Gani": 99
+#  }
+# for t,e, in f.items():
+#     if t == "Eng yuqori ball":
+#         print(f"{t}")
+#         for ball in e:
+#             print(f"   -{ball}")
+#     else:
+#         print(f"{t} {e}")
+
+
+# print("Gani")
+
+
+    # 2-topshiriq:
+
+# mevalar = {"olma": 5, "anor": 3, "gilos": 10}
+# print(mevalar.get("nok", 0))
+
+# talaba = {"ism": "Ali", "yosh": 20} 
+# talaba.update({"kurs": 2, "yosh": 21})
+# print(talaba)
+
+
+# kitoblar = {
+# "Python" : "Guido van Rossum",
+# "Java" : "James Gosling",
+# "C++" : "Bjarne Stroustrup"
+
+# }
+# print(kitoblar)
+
+
+    # 3-topshiriq:
+
+# f = {
+# "Ali": 85, 
+# "Vali": 92, 
+# "Hasan": 78,
+#  "Gani": 99
+#  }
+# print("Gani")
+
+
+
+
+
+# Ro'yxat elementlari:
+# f = {
+# "brand":"Apple",
+# "model":"iPhone",
+# "year": 1977,
+# "color":"black"
+# }
+# print(f)
+
+# f = {
+# "brand":"Apple",
+# "model":"iPhone",
+# "year": 1977,
+# "color":"black"
+# }
+# print(f)
+
+
+
+
+
+
+
+# 3-topshiriq
+talabalar = {
+    "Ali": {
+        "yosh": 20,
+        "kurs": 2,
+        "universitet": "TATU"
+        },
+    "Vali": {
+        "yosh": 21,
+        "kurs": 3, 
+        "universitet": "SAMDU"
+        }
+}
+print(talabalar)
+print(talabalar["Ali"]["yosh"])
+print(talabalar["Vali"]["universitet"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
