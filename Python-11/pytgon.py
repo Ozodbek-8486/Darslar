3c4ba5e874e1dbbb5ba043224b94d99af856c4d5
ozodbek7767 <ozodbekusmonqulov7777@gmail.com>
2025-02-26 16:40:34 +0500

    f
    f

---
+++ Python-11/pytgon.py
@@ -0,0 +1,119 @@
+# 1-topshiriq
+# dasturchi = {
+#     "Ozodbek":['HTML,', 'CSS,', 'JS', 'Python'],
+
+# }
+# for ism, tillar in dasturchi.items():
+#     print(f"\n{ism.title()}Men quyadigan dasturlash tilarini bilaman.:")
+# for til in tillar:
+#     print(f"- {til}")
+
+# 2-topshiriq
+# kitoblar = []
+# for n in range(3):
+
+#     kitob = {
+#         "nomi":"Forsaj",
+#         "year":2001,
+#         "egasi":"Fast & Furious"
+# }
+#     kitob1 = {
+#         "nomi":"Harri Poter",
+#         "year":2001,
+#         "egasi":"J.K.Rowling"
+#         }
+#     kitob2 ={
+#         "nomi":"Titanik",
+#         "year":1997,
+#         "egasi":"James Cameron"
+#         }
+#     kitob3 = {
+#         "nomi":"Avatar",
+#         "year":2009,
+#         "egasi":"James Cameron"
+#         }
+#     kitob4 = {
+#         "nomi":"Uzuklar Hukmdori",
+#         "year":2001,
+#         "egasi":"J.K.Rowling"
+#         }
+#     kitob5 = {
+#         "nomi":"Kung Fu Panda",
+#         "year":2008,
+#         "egasi":"Mark Osborne"
+#         }
+
+# # 1- kitob
+# print("1-Kitob")
+# kitob = kitob1
+# print(f"Nomi: {kitob['nomi']}")
+# print(f"{kitob['year']} yil")
+# print(f"Egasi: {kitob['egasi']}\n")
+
+# # 2-kitob
+# print("2-Kitob")
+# kitob = kitob2
+# print(f"Nomi: {kitob['nomi']}")
+# print(f"{kitob['year']} yil")
+# print(f"Egasi: {kitob['egasi']}\n")
+
+# # 3-kitob
+# print("3-Kitob")
+# kitob = kitob3
+# print(f"Nomi: {kitob['nomi']}")
+# print(f"{kitob['year']} yil")
+# print(f"Egasi: {kitob['egasi']}\n")
+
+# # 4-kitob
+# print("4-Kitob")
+# kitob = kitob5
+# print(f"Nomi: {kitob['nomi']}")
+# print(f"{kitob['year']} yil")
+# print(f"Egasi: {kitob['egasi']}\n")
+
+# #  5-kitob
+# print("5-Kitob")
+# kitob = kitob5
+# print(f"Nomi: {kitob['nomi']}")
+# print(f"{kitob['year']} yil")
+# print(f"Egasi: {kitob['egasi']}\n")
+
+
+
+# 3-topshiriq
+
+
+# cars = []
+# car1 = {
+#        "company": "Mersedec Benz",
+#        "model":"CLS63 AMG", 
+#        "year":2006,
+#        "price": 35000,
+#        "color": "Qora"
+#        }
+# car2 = {
+#        "company": "Lamborgini",
+#        "model":"Lamborgini Huracan",
+#        "year":2024, 
+#        "price": 809000,
+#        "color": "Oq"
+#        }
+# car3 = {
+#          "company": "Ford",
+#          "model":"Mustang GTD",
+#          "year":2025,
+#          "price": 400000,
+#          "color": "Qizil"
+#          }
+# car4 = {
+#          "company": "Rolly Royce",
+#          "model":"Droptail",
+#          "year":2023,
+#          "price": 700000,
+#          "color": "ko'k"
+#          }
+ 
+# print(f"\n{car1['company']},{car1['model']}, {car1['color']} rangli, narxi {car1['price']}$ dollar\n")
+# print(f"{car2['company']},{car2['model']}, {car2['color']} rangli, narxi {car2['price']}$ dollar\n")
+# print(f"{car3['company']},{car3['model']}, {car3['color']} rangli, narxi {car3['price']}$ dollar\n")
+# print(f"{car4['company']},{car4['model']}, {car4['color']} rangli, narxi {car4['price']}$ dollar\n")
